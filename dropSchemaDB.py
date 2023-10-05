ba3dfb68d4d0edbffca98cb344190140b5c2ece4
Diego Banchero <diegobanchero@Diegos-MBP.homenet.telecomitalia.it>
2023-10-05 23:20:17 +0200

    fixed configurations

--- dropSchemaDB.py
+++ dropSchemaDB.py
@@ -6,13 +6,17 @@ import weaviate
 load_dotenv()
 
 open_ai_key = os.getenv("OPENAI_API_KEY")
+weaviate_url = os.getenv("WEAVIATE_API_URL")
+weaviate_ai_key = os.getenv("WEAVIATE_API_KEY")
+
 client = weaviate.Client(
-    url = "http://localhost:8081",  # Replace with your endpoint
-    auth_client_secret=weaviate.AuthApiKey(api_key=""),  # Replace w/ your Weaviate instance API key
+    url = weaviate_url,  # Replace with your endpoint
+    auth_client_secret=weaviate.AuthApiKey(api_key=weaviate_ai_key),  # Replace w/ your Weaviate instance API key
     additional_headers = {
         "X-OpenAI-Api-Key": open_ai_key  # Replace with your inference API key
     }
 )
+
 
 
 client.schema.delete_class("StartupUserCase")
